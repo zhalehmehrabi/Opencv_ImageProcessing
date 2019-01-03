cc516ccc5a92ad96a3f6dc0a88fb9aba53790256
zhalehmehraby <a.h.zhalehmehraby@gmail.com>
2019-01-03 23:28:18 +0330

    third

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,8 +1,10 @@
 import cv2 as cv
 import numpy as np
+import time
 
-img = cv.imread('street.jpg', -1)
+img = cv.imread('p.jpg', -1)
 rows, cols, channel = img.shape
+print(rows)
 # 1
 cv.imshow('image', img)
 
@@ -34,7 +36,7 @@ if k == ord('5'):
 
 # 6
 if k == ord('6'):
-    scaled = cv.resize(img, (int(rows / 2), cols), interpolation=cv.INTER_CUBIC)
+    scaled = cv.resize(img, (int(cols / 2), rows), interpolation=cv.INTER_AREA)
     cv.imshow('scaled', scaled)
     cv.waitKey(0)
 # 7
@@ -44,29 +46,78 @@ if k == ord('7'):
     cv.waitKey(0)
 
 # 8
-if k == ord('8'): # mn ba gaussian zdm ta behtar maloom she
+if k == ord('8'):  # mn ba gaussian zdm ta behtar maloom she
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    grayAfterGaussian = cv.GaussianBlur(gray, (5, 5), 0)
-    ret, thresh = cv.threshold(grayAfterGaussian, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+    ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
     # noise removal
     kernel = np.ones((3, 3), np.uint8)
-    opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2)
+    opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=3)
     # sure background area
-    sure_bg = cv.dilate(opening, kernel, iterations=3)
-    cv.imshow('sure', sure_bg)
-
+    sure_bg = cv.dilate(opening, kernel, iterations=5)
     # Finding sure foreground area
     dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)
-    ret, sure_fg = cv.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
-    # Finding unknown region
+    ret, sure_fg = cv.threshold(dist_transform, 0.5 * dist_transform.max(), 255, 0)
     sure_fg = np.uint8(sure_fg)
     unknown = cv.subtract(sure_bg, sure_fg)
-    # Marker labelling
     ret, markers = cv.connectedComponents(sure_fg)
-    # Add one to all labels so that sure background is not 0, but 1
     markers = markers + 1
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
     markers = cv.watershed(img, markers)
     img[markers == -1] = [255, 0, 0]
-
-    cv.imshow('scaled', thresh)
+    cv.imshow('segmented', img)
     cv.waitKey(0)
+
+# 9
+if k == ord('9'):
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1, 5)
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+    cv.imshow('img', img)
+# 10
+if k == ord('0'):
+    i = 0
+    cap = cv.VideoCapture('output.avi')
+
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv.imshow('frame', frame)
+        cv.waitKey(500)
+        if i == 5:
+            break
+        i += 1
+    cap.release()
+    cv.destroyAllWindows()
+
+if k ==ord('r'):
+    cap = cv.VideoCapture(0)
+    # Define the codec and create VideoWriter object
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            frame = cv.flip(frame, 0)
+            # write the flipped frame
+            rows, cols, channel = frame.shape
+            M = cv.getRotationMatrix2D(((cols - 1) / 2.0, (rows - 1) / 2.0), 180, 1)
+            dst = cv.warpAffine(frame, M, (cols, rows))
+            out.write(dst)
+            cv.imshow('frame', dst)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            break
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
